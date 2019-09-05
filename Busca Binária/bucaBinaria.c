d673447c2fa44f551df4a821ff2de0961e96e30b
Lucas Sargeiro Gomes de Mello <sargeirolucas@gmail.com>
2019-09-04 22:55:54 -0300

    Update bucaBinaria.c

--- Busca Binária/bucaBinaria.c
+++ Busca Binária/bucaBinaria.c
@@ -1,62 +1,5 @@
-/*#include <stdio.h>
-#include <string.h>
-
-typedef struct _Endereco Endereco;
-
-struct _Endereco{
-
-	char logradouro[72];
-	char bairro[72];
-	char cidade[72];
-	char uf[72];
-	char sigla[2];
-	char cep[8];
-	char lixo[2];
-
-};
-
-int main(int argc, char**argv){
-
-	FILE *f;
-	Endereco e;
-	int qt;
-	int c;
-
-	if(argc != 2){
-
-		fprintf(stderr, "USO: %s [CEP]", argv[0]);
-		return 1;
-
-	}
-
-    printf("CEP utilizado: %s\n", argv[1]);
-
-	c = 0;
-	printf("Tamanho da Estrutura: %ld\n\n", sizeof(Endereco));
-	
-	f = fopen("cep_ordenado.dat","r");
-	qt = fread(&e,sizeof(Endereco),1,f);
-
-	while(qt > 0){
-
-		c++;
-		
-		if(strncmp(argv[1],e.cep,8)==0){
-
-			printf("%.72s\n%.72s\n%.72s\n%.72s\n%.2s\n%.8s\n",e.logradouro,e.bairro,e.cidade,e.uf,e.sigla,e.cep);
-			break;
-
-		}
-	
-		qt = fread(&e,sizeof(Endereco),1,f);		
-	
-	}
-
-	printf("Total Lido: %d\n", c);
-	fclose(f);
-}*/
-
 #include <stdio.h>
+#include <string.h>
 
 typedef struct _Endereco Endereco;
 
@@ -71,31 +14,125 @@ struct _Endereco
 	char lixo[2];
 };
 
-int main(int argc, char**argv)
-{
-	FILE *f;
-	Endereco e;
-	int qt;
-	long posicao, primeiro, ultimo, meio;
+/*
+	SEEK_SET = Posição inicial	
+	SEEK_CUR = Posição atual
+	SEEK_END = Posição final
 
-	if(argc != 2)
-	{
-		fprintf(stderr, "USO: %s [CEP]", argv[0]);
-		return 1;
+*/
+
+
+int buscaBinaria(FILE *f, char *bCep,long ini,long fim,int cont){  // Retorna o numero de iterações (cont)
+
+	Endereco e;
+	long meio;
+	rewind(f);												// Volta ao começo do arquivo
+	if(ini <= fim){
+
+		int qt;	// Testa se foi possivel ler
+
+		cont++;
+
+		meio = (ini+fim)/2;
+
+		//printf("\nINICIO: %ld\tMEIO: %ld\tFINAL: %ld\n",ini,meio,fim);
+
+		fseek(f,(meio*sizeof(Endereco)),SEEK_SET);
+
+		qt = fread(&e, sizeof(Endereco), 1, f);
+		//printf("\n\nLogradouro: %.72s\nBairro: %.72s\nCidade: %.72s\nUF: %.72s\nSigla: %.2s\nCEP: %.8s\n",e.logradouro,e.bairro,e.cidade,e.uf,e.sigla,e.cep);
+
+		if(strncmp(bCep,e.cep,8) == 0){
+			printf("\nLogradouro: %.72s\nBairro: %.72s\nCidade: %.72s\nUF: %.72s\nSigla: %.2s\nCEP: %.8s\n\n",e.logradouro,e.bairro,e.cidade,e.uf,e.sigla,e.cep);
+			return cont;
+		}
+		else{
+			if(strncmp(bCep,e.cep,8) > 0){
+				buscaBinaria(f,bCep,meio+1,fim,cont);
+			}
+			else if(strncmp(bCep,e.cep,8) < 0){
+				buscaBinaria(f,bCep,ini,meio-1,cont);
+			}
+
+		}
+
 	}
-	f = fopen("cep_ordenado.dat","r");
-	fseek(f,0,SEEK_END);
-	posicao = ftell(f);
-	rewind(f);
-	printf("Tamanho do Arquivo: %ld\n", posicao);
+	else{
+		
+		printf("Não foi possivel encontrar o CEP\n");
+		return cont;
+
+	}
+
+}
+
+int buscaNaoBinaria(FILE *f, char *bCep,int cont){
+
+	rewind(f);												// Volta ao começo do arquivo
+	Endereco e;
+	int qt;		// (qt) Armazena quantos foram lidos
+	qt = fread(&e,sizeof(Endereco),1,f);
+
+	while(qt > 0){
+
+		cont++;
+		if(strncmp(bCep,e.cep,8) == 0){
+			printf("\n\nLogradouro: %.72s\nBairro: %.72s\nCidade: %.72s\nUF: %.72s\nSigla: %.2s\nCEP: %.8s\n\n",e.logradouro,e.bairro,e.cidade,e.uf,e.sigla,e.cep);
+			break;
+
+		}
+	
+		qt = fread(&e,sizeof(Endereco),1,f);		
+	
+	}
+
+	return cont;
+
+}
+
+int main(int argc, char**argv){
+	
+	FILE *f;								
+	long posicao, primeiro, ultimo, meio;
+	char bCep[8];
+
+	if(argc != 2){											// Verifica passagem de parâmetro
+		printf("Entre com o CEP que deseja pesquisar:\n");
+		int i = 0;
+		while(i < 8){
+			bCep[i++] = getchar();
+		}
+	}
+	else{
+
+		strncpy(bCep,argv[1],8);
+
+	}
+
+	f = fopen("cep_ordenado.dat","r");						// Abertura de arquivo
+
+
+	fseek(f,0,SEEK_END);									// Navega até o final do arquivo
+	posicao = ftell(f);										// Descobre o tamanho do arquivo
+	
+
+
+	printf("________________________[INFO]________________________\n\n");
+	printf("Tamanho do Arquivo: %ldBytes\n", posicao);
 	printf("Tamanho do Registro: %ld\n", sizeof(Endereco));
 	printf("Tamanho do Arquivo em Registros: %ld\n", posicao/sizeof(Endereco));
+	
 	primeiro = 0;
 	ultimo = (posicao/sizeof(Endereco))-1;
-	meio = (primeiro+ultimo)/2;
 
-	rewind(f);
+	
+	printf("\n___________________[BUSCA BINÁRIA]____________________\n\n");
+	printf("Numero de iterações: %d\n",buscaBinaria(f, bCep,primeiro, ultimo,0));
+	
+
+	printf("\n_________________[BUSCA NÃO BINÁRIA]__________________\n\n");
+	printf("Numero de iterações: %d\n",buscaNaoBinaria(f, bCep,0));
+	printf("______________________________________________________\n\n");
 
 	return 0;
 }
-
